5d3b3c9e2e9377a5519600574e43bfce8998d687
Brendan Schatz <brendanschatz97@gmail.com>
2019-11-01 11:49:23 -0400

    got my custom hooks made

--- client/src/PlayerList.jsx
+++ client/src/PlayerList.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+width: 50%;
+background: green;
+color: purple;
+
+`
 
 
 export default function PlayerList(props){
     return (
-        null
+        <Card>
+            <h1>Name: {props.name}</h1>
+            <h3>Country: {props.country}</h3>
+            <h3>Searches: {props.searches}</h3>
+        </Card>
     )
 }
